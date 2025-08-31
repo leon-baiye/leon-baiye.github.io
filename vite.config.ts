1fb188899ae32347944924f6daab08d3ad1420d3
Leon Baiye <118029914+leon-baiye@users.noreply.github.com>
2025-08-30 20:30:07 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/leon-baiye.github.io/',
   server: {
     host: "::",
     port: 8080,
